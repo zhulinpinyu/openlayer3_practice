dde9269571bb2edeb1a2e6ba33c22cf386315e4c
zhulinpinyu <mulixiang199016@gmail.com>
2015-04-16 02:00:47 +0800

    use GeoJson

--- hi_ol/init.js
+++ hi_ol/init.js
@@ -1,4 +1,4 @@
-var map,baselayer,vectorLayer,vectorLayer2;
+var map,baselayer,vectorLayer,vectorLayer2,styleCache,geoLayer;
 
 function init(){
   map = new ol.Map({
@@ -35,11 +35,49 @@ function init(){
       }
     }),
   });
+
+  styleCache = {};
+  geoLayer = new ol.layer.Vector({
+    source: new ol.source.GeoJSON({
+      projection: 'EPSG:900913',
+      url: './myGeoJson.json'
+    }),
+    style: function(feature,resolution){
+      var text = resolution < 5000 ? feature.get('name') : ''
+      if(!styleCache[text]){
+        styleCache[text] = [new ol.style.Style({
+          fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,0.1)'
+          }),
+          stroke: new ol.style.Stroke({
+            color: '#319FD3',
+            width: 1
+          }),
+          text: new ol.style.Text({
+            font: '12px Calibri, sans-serif',
+            text: text,
+            fill: new ol.style.Fill({
+              color: '#000'
+            }),
+            stroke: new ol.style.Stroke({
+              color: '#fff',
+              width: 3
+            })
+          }),
+          zIndex: 999
+        })];
+      }
+      return styleCache[text];
+    }
+  });
+
+
   //设置透明度
   //vectorLayer.setOpacity(.3);
   map.addLayer(baselayer);
-  map.addLayer(vectorLayer);
+  //map.addLayer(vectorLayer);
   map.addLayer(vectorLayer2);
+  map.addLayer(geoLayer);
 }
 
 function removeTopLayer(){
