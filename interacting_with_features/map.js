777df5b2690671931e8dde28e2c49b6f57f1934f
zhulinpinyu <mulixiang199016@gmail.com>
2015-04-21 09:09:06 +0800

    显示上海各个区县的区域

--- interacting_with_features/map.js
+++ interacting_with_features/map.js
@@ -1,20 +1,24 @@
+var baseLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
 var countries = new ol.layer.Vector({
   source: new ol.source.GeoJSON({
     projection: 'EPSG:3857',
-    url: '../data/countries.geojson'
+    url: '../data/shanghai.json'
   })
 });
 
-var center = ol.proj.transform([0,0],'EPSG:4326','EPSG:3857');
+var center = ol.proj.transform([121.49,31.201],'EPSG:4326','EPSG:3857');
 
 var view = new ol.View({
   center: center,
-  zoom: 1
+  zoom: 9
 });
 
 var map = new ol.Map({
   target: 'map',
-  layers: [countries],
+  layers: [baseLayer,countries],
   view: view
 });
 
