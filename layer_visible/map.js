d362e1c922d94c9ee5b5845e73c570bf39d127a4
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-05 17:56:07 +0800

    add baidu_map layer

--- layer_visible/map.js
+++ layer_visible/map.js
@@ -1,18 +1,26 @@
-var layer = new ol.layer.Tile({
+var osmlayer = new ol.layer.Tile({
   source: new ol.source.OSM()
+});
+
+var baidulayer = new ol.layer.Tile({
+  source: tilesource
 });
 
 var shenzhen = ol.proj.transform([114.01,22.51],'EPSG:4326','EPSG:3857');
 var view = new ol.View({
   center: shenzhen,
-  zoom: 7
+  zoom: 12
 });
 
 var map = new ol.Map({
   target: 'map',
-  layers: [layer],
+  layers: [osmlayer,baidulayer],
   view: view
 });
 
-var visible = new ol.dom.Input($("#visible")[0]);
-visible.bindTo('checked', layer, 'visible');
+$(document).ready(function(){
+  var osm = new ol.dom.Input($("#osm")[0]);
+  osm.bindTo('checked', osmlayer, 'visible');
+  var baidu = new ol.dom.Input($("#baidu")[0]);
+  baidu.bindTo('checked', baidulayer, 'visible');
+});
