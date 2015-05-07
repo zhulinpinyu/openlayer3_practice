d6043aaa16bb0c2419b965b04f4e740cd2e81e50
zhulinpinyu <mulixiang199016@gmail.com>
2015-05-07 10:29:34 +0800

    add sz_stores heatmap

--- mobile/ol3map/js/map.js
+++ mobile/ol3map/js/map.js
@@ -9,6 +9,16 @@ var baseLayer = new ol.layer.Tile({
 //   })
 // });
 
+var sz_stores_layer = new ol.layer.Heatmap({
+  source: new ol.source.GeoJSON({
+    url: 'data/sz_stores.geo.json',
+    projection: 'EPSG:3857'
+  }),
+  blur: 8,
+  radius: 10,
+  opacity: 0.6
+});
+
 var center = ol.proj.transform([114.02,22.54],'EPSG:4326','EPSG:3857');
 //-122.0312186,37.33233141
 var view = new ol.View({
@@ -18,7 +28,7 @@ var view = new ol.View({
 
 var map = new ol.Map({
   target: 'map',
-  layers: [baseLayer],
+  layers: [baseLayer,sz_stores_layer],
   view: view,
   controls: []
 });
@@ -37,8 +47,9 @@ function onMouseMove(event){
   var degrees = ol.proj.transform(coordinate, 'EPSG:3857','EPSG:4326');
   var hdms = ol.coordinate.toStringHDMS(degrees)
   map.forEachFeatureAtPixel(pixel, function(feature){
-    var value = feature.get('name')+"("+hdms+")"
-    document.location = "ol3map://alert/"+value;
+    console.log(feature.get('lgl_biz_name'));
+    //var value = feature.get('name')+"("+hdms+")"
+    //document.location = "ol3map://alert/"+value;
   });
 }
 map.on('click', onMouseMove);
