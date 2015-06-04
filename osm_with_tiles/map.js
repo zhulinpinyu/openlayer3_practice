1809f8348ab9e97f13454512b42e76548365aa9e
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-04 17:31:53 +0800

    Tile Amap

--- osm_with_tiles/map.js
+++ osm_with_tiles/map.js
@@ -1,6 +1,7 @@
 var layer = new ol.layer.Tile({
   source: new ol.source.XYZ({
-    url: "http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //url: "http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    url: "http://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}"
   })
 });
 
@@ -8,7 +9,9 @@ var center = ol.proj.transform([114.02,22.534],'EPSG:4326','EPSG:3857');
 
 var view = new ol.View({
   center: center,
-  zoom: 14
+  zoom: 14,
+  maxZoom: 18,
+  minZoom: 4
 });
 
 var map = new ol.Map({
