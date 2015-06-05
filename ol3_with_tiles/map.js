fe8b43bca944e3f98c56dca0d4091fb2b14685c4
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-05 10:23:50 +0800

    add Google Tile

--- ol3_with_tiles/map.js
+++ ol3_with_tiles/map.js
@@ -1,14 +1,20 @@
-var osm_layer = new ol.layer.Tile({
+var local_osm_layer = new ol.layer.Tile({
   source: new ol.source.XYZ({
     url: "http://10.211.55.25/osm/{z}/{x}/{y}.png"
     //"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    //http://10.211.55.24/osm/8/213/111.png
   })
 });
 
 var amap_layer = new ol.layer.Tile({
   source: new ol.source.XYZ({
     url: "http://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}"
+  })
+});
+
+var google_layer = new ol.layer.Tile({
+  source: new ol.source.XYZ({
+    url: "http://mt1.google.cn/vt/lyrs=m@142&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}&s=Galil"
+    //Options: mt0.google.cn mt1.google.cn mt2.google.cn mt3.google.cn
   })
 });
 
@@ -23,6 +29,6 @@ var view = new ol.View({
 
 var map = new ol.Map({
   target: 'map',
-  layers: [amap_layer],
+  layers: [google_layer],
   view: view
 });
