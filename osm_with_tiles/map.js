76be4b65b1491a7c4f7507b5ce2b8b2958e698f0
zhulinpinyu <mulixiang199016@gmail.com>
2015-06-05 09:34:46 +0800

    local osm tile layer

--- osm_with_tiles/map.js
+++ osm_with_tiles/map.js
@@ -1,7 +1,8 @@
 var osm_layer = new ol.layer.Tile({
   source: new ol.source.XYZ({
-    url: "http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    //url: "http://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}"
+    url: "http://10.211.55.25/osm/{z}/{x}/{y}.png"
+    //"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //http://10.211.55.24/osm/8/213/111.png
   })
 });
 
@@ -11,17 +12,17 @@ var amap_layer = new ol.layer.Tile({
   })
 });
 
-var center = ol.proj.transform([114.025044,22.531192],'EPSG:4326','EPSG:3857');
+var center = ol.proj.transform([114.026,22.532],'EPSG:4326','EPSG:3857');
 
 var view = new ol.View({
   center: center,
-  zoom: 14,
+  zoom: 17,
   maxZoom: 18,
   minZoom: 4
 });
 
 var map = new ol.Map({
   target: 'map',
-  layers: [osm_layer, amap_layer],
+  layers: [amap_layer],
   view: view
 });
